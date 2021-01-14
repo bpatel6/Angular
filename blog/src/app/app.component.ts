9ad42c474f6673f42f9266954197a94c419b117d
bpatel6 <bpatel6@uiowa.edu>
2021-01-13 18:02:28 -0600

    basic functionality of Angular

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'blog';
+  name = 'Bhavik';
+  getName(){
+    return 'Suku'
+  }
+  obj = {
+    name: 'Kishan',
+    age: 21
+  }
+  arr = ['Bruce', 'Swapnil'];
+  siteURL = window.location.href;
 }
